3219144921130d2b3cba85a784c219ae11bdce07
Fatima Rizvi <fatimarzv23@gmail.com>
2021-02-01 12:03:39 -0600

    Created a folder of dummy data to pass to mocki.io

---
+++ dummyData.js
@@ -0,0 +1,55 @@
+// Dummy data to pass to mocki.io when creating a fake api
+const women = [
+    {
+        name: 'Grace Hopper',
+        born: 'December 9, 1906',
+        died: 'January 1, 1992',
+        STEM: ['technology'],
+        field: ['computer science'],
+        blurb: 'pioneer of computer programming',
+        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum',
+        sources: ['https://en.wikipedia.org/wiki/Grace_Hopper#Legacy']
+    },
+    {
+        name: 'Marie Curie',
+        born: 'November 7, 1867',
+        died: 'July 4, 1934',
+        STEM: ['science'],
+        fields: ['radioactivity', 'physics', 'chemistry'],
+        blurb: 'conducted pioneering research on radioactivity',
+        description: 'lorem ipsum she was the first woman to win a Nobel Prize, the first person and the only woman to win the Nobel Prize twice, and the only person to win the Nobel Prize in two scientific fields. She was the first woman to become a professor at the University of Paris in 1906. lorem ipsum',
+        sources: ['https://interestingengineering.com/10-greatest-women-stem', 'https://en.wikipedia.org/wiki/Marie_Curie']
+    },
+    {
+        name: 'Rosalind Franklin',
+        born: 'July 25, 1920',
+        died: 'April 16, 1958',
+        STEM: ['science'],
+        field: ['physical chemistry', 'x-ray crystallography','DNA'],
+        blurb: 'best known for her work on X-Ray diffraction images of DNA',
+        description: 'lorem ipsum Her infamous Photo 51 led to the discovery of the double helix structure by Watson, Crick, and Wilkins in 1962. lorem ipsum.',
+        sources: ['https://interestingengineering.com/10-greatest-women-stem', 'https://en.wikipedia.org/wiki/Rosalind_Franklin']
+    },
+    {
+        name: 'Ada Lovelace',
+        born: 'December 10, 1815',
+        died: 'November 27, 1852',
+        STEM: ['math'],
+        field: ['computer programming', 'algortims', 'computing'],
+        blurb: 'the very first computer programmer',
+        description: 'lorem ipsum published the first algorithm intended to be carried out by such a machine. lorem ipsum.',
+        sources: ['']
+    }
+]
+
+// Empty object
+// {
+//     name: '',
+//     born: '',
+//     died: '',
+//     STEM: [''],
+//     field: [''],
+//     blurb: '',
+//     description: '',
+//     sources: ['']
+// }
